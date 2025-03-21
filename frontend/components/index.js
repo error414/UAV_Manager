db5063c13a13d6e618e0fdbc7414c2e4377dfa8e
Jonatan Carvalhais <74775121+JohnnyCarvi@users.noreply.github.com>
2025-03-21 10:47:19 +0100

    Update index.js

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -1,5 +1,5 @@
 export { default as AuthLayout } from './AuthLayout';
-export { default as FormInput } from './formInput';
+export { default as FormInput } from './FormInput';
 export { default as Alert } from './Alert';
 export { default as Button } from './Button';
 export { default as Filters } from './Filters';
